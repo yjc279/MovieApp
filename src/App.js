e57fe1461c0d21149dd4e830d9e88dbecb01f6fe
YJC mac <yoon_je_chung@solomonsc.com>
2018-06-06 22:05:04 +0900

    more parts

--- src/App.js
+++ src/App.js
@@ -2,12 +2,35 @@ import React, { Component } from 'react';
 import Movie from './Movie.js';
 import './App.css';
 
+
+const movies = [
+  {
+    title: "Matrix",
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
+  },
+  {
+    title: "Oldboy",
+    poster:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
+  },
+  {
+    title: "StarWars",
+    poster:"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/1280px-Star_Wars_Logo.svg.png"
+  },
+  {
+    title: "StarTrek",
+    poster: "http://www.startrekmovie.com/images/share.jpg"
+  }
+]
+
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <Movie />
-      </div>
+    return (<div className="App">
+      {movies.map(movie => {
+        return <Movie title={movie.title} poster={movie.poster} />
+      })}
+    </div>
     );
   }
 }
--- src/Movie.js
+++ src/Movie.js
@@ -3,18 +3,20 @@ import './Movie.css';
 
 class Movie extends Component{
     render() {
-        return(
-        <div>
-            <MoviePoster />
-            <h1>hello this is a movie</h1>
-        </div>
+        return (
+            <div>
+                <MoviePoster poster={this.props.poster} />
+                <h1>{this.props.title}</h1>
+            </div>
         );
+
+
     }
 }
 
 class MoviePoster extends Component {
     render() {
-        return <img src="https://profilesinhistory.com/wp-content/uploads/2013/11/hungerGames.png" />;
+        return <img src={this.props.poster} />; 
     }
 }
 export default Movie;
