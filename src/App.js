2d7ea4e2f2fcb891c4e8d054560500f746bc1983
YJC mac <yoon_je_chung@solomonsc.com>
2018-06-07 01:46:32 +0900

    loading state

--- src/App.js
+++ src/App.js
@@ -4,45 +4,48 @@ import './App.css';
 
 class App extends Component {
   state = {
-    greeting: "Hello!",
-    movies: [
-      {
-        title: "Matrix",
-        poster:
-          "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
-      },
-      {
-        title: "Oldboy",
-        poster:
-          "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
-      },
-      {
-        title: "StarWars",
-        poster: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/1280px-Star_Wars_Logo.svg.png"
-      },
-      {
-        title: "StarTrek",
-        poster: "http://www.startrekmovie.com/images/share.jpg"
-      }
-    ]
+    
   }
   componentDidMount(){
    setTimeout(() => {
      this.setState({
        movies: [
-         ...this.state.movies,{
-           title:"StarWars2",
+         {
+           title: "Matrix",
+           poster:
+             "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
+         },
+         {
+           title: "Oldboy",
+           poster:
+             "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
+         },
+         {
+           title: "StarWars",
+           poster: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/1280px-Star_Wars_Logo.svg.png"
+         },
+         {
+           title: "StarTrek",
+           poster: "http://www.startrekmovie.com/images/share.jpg"
+         },
+         {
+           title: "StarWars2",
            poster: "http://www.startrekmovie.com/images/share.jpg"
          }
-       ] 
+       ]
      })
-   }, 3000);
+   }, 5000);
   }
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    })
+    return movies
+  }
+
   render() {
     return (<div className="App">
-      {this.state.movies.map((movie,index) => {
-        return <Movie title={movie.title} poster={movie.poster} key={index}/>
-      })}
+      {this.state.movies ? this._renderMovies() : "Loading"}
     </div>
     );
   }
