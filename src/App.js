4e9d525a91b52968a445219f55f1862aef025d1f
YJC mac <yoon_je_chung@solomonsc.com>
2018-06-08 02:03:27 +0900

    finished

--- src/App.js
+++ src/App.js
@@ -9,7 +9,14 @@ class App extends Component {
   }
   _renderMovies = () => {
     const movies = this.state.movies.map(movie => {
-      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
+      console.log(movie)
+      return <Movie 
+        title={movie.title_english}
+        poster={movie.medium_cover_image}
+        key={movie.id}
+        genres={movie.genres}
+        synopsis={movie.synopsis} 
+        />
     })
     return movies
   }
@@ -20,14 +27,15 @@ class App extends Component {
     })
   }
   _callApi = () => {
-    return fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating")
+    return fetch("https://yts.am/api/v2/list_movies.json?sort_by=download_count")
       .then(response => response.json())
       .then(json => json.data.movies)
       .catch(err => console.log(err))
   }
 
   render() {
-    return (<div className="App">
+    const { movies } = this.state; 
+    return (<div className={movies ? "App" : "App--loading"}> 
       {this.state.movies ? this._renderMovies() : "Loading"}
     </div>
     );
